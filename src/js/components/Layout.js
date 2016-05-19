10869a2eab01c0afa9cebac00e35816345f07c10
leslie kimm <leslie.kimm@gmail.com>
2016-05-19 15:42:28 -0700

    Use state to change name

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,9 +4,19 @@ import Footer from "./Footer";
 import Header from "./Header"; 
 
 export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = {name: "Leslie"}
+  }
+
   render() {
+    setTimeout(() => {
+      this.setState({name: "Bob"});
+    }, 1000)
     return (
+      
       <div>
+      {this.state.name}
         <Header />
         <Footer />
       </div>
