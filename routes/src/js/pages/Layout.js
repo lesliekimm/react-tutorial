6d74e8ddeba2186cedd1f3b2aeea7c546e20e1fa
leslie kimm <leslie.kimm@gmail.com>
2016-05-19 20:10:33 -0700

    add routes

--- routes/src/js/pages/Layout.js
+++ routes/src/js/pages/Layout.js
@@ -5,30 +5,19 @@ import Footer from "../components/layout/Footer";
 import Nav from "../components/layout/Nav";
 
 export default class Layout extends React.Component {
+  navigate() {
+    this.props.history.pushState(null, "/");
+  }
+
   render() {
-    const { location } = this.props;
-    const containerStyle = {
-      marginTop: "60px"
-    };
-    console.log("layout");
     return (
-      <div>
-
-        <Nav location={location} />
-
-        <div class="container" style={containerStyle}>
-          <div class="row">
-            <div class="col-lg-12">
-              <h1>KillerNews.net</h1>
-
-              {this.props.children}
-
-            </div>
-          </div>
-          <Footer/>
-        </div>
+      <div className="container">
+        <h1>KillerNews.net</h1>
+        <Link to="archives"><button class="btn btn-danger">archives</button></Link>
+        <Link to="settings"><button class="btn btn-success">settings</button></Link>
+        <button onClick={this.navigate.bind(this)}>featured</button>
+        {this.props.children}
       </div>
-
     );
   }
 }
